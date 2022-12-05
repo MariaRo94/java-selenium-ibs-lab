8ecea3eb0027c3b44df12c72540c9937bbeaba6e
Michael Angelo Arizala <michaelangelo.arizala@gmail.com>
2022-12-05 16:36:49 +1100

    Call to environment variable.

--- src/test/java/web_saucedemo/tests/testng/testcases/BaseTest.java
+++ src/test/java/web_saucedemo/tests/testng/testcases/BaseTest.java
@@ -5,22 +5,21 @@ import automation.selenium.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import web_saucedemo.config.EnvironmentVariables;
 
 import java.time.Duration;
 
 public abstract class BaseTest {
 
-    // TODO: Externalise data
+    // TODO: Externalise config data
     protected WebDriver driver;
     private Browsers browser = Browsers.CHROME;
-    private int implicitWait = 15;
-
     private String url = "https://www.saucedemo.com/";
 
     @BeforeMethod
     public void setup() {
         driver = BrowserFactory.launch(browser);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(EnvironmentVariables.WAIT_IMPLICIT));
         driver.manage().window().maximize();
         driver.get(url);
     }
